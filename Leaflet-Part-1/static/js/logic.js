e0a5a7c636cf1661f942b3582f79536247836fa5
kirsty185 <126469546+kirsty185@users.noreply.github.com>
2023-07-11 18:22:19 +0800

    Add files via upload
    initial from local repository

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,93 @@
+//url : https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+// Perform an API call to the Earthquakes API to get the earthquake information. Call createMarkers when it completes.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+
+function createMap(earthquakes) {
+// Create the base layers.
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Create a baseMaps object.
+let baseMaps = {
+  "Street Map": streetmap,
+ 
+};
+
+
+// Create an overlayMaps object to hold the earthquake layer.
+let overlayMaps = {
+  "Earthquake": earthquakes
+};
+
+let map = L.map("map", {
+  center: [20.5937, 78.9629],
+  zoom: 3,
+  layers: [streetmap, earthquakes]
+});
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(map);
+
+// Add the legend with colors to corrolate with depth
+let info = L.control({position: "bottomright"});
+info.onAdd = function() {
+  let div = L.DomUtil.create("div", "legend"),
+  depth = [-10, 10, 30, 50, 70, 90];
+
+  div.innerHTML = "<h3 style='text-align: center'>Depth</h3>"
+
+  for (let i = 0; i < depth.length; i++) {
+    div.innerHTML +=
+    '<i style="background-color:' + chooseColor(depth[i] + 1) + '"></i>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+  }
+  return div;
+};
+
+info.addTo(map);
+
+}
+// Function to determine marker color by depth
+function chooseColor(depth){
+  if (depth < 10) return "green";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "purple";
+}
+
+
+function createMarkers(response) {
+
+  // Pull the "features" property from response.
+  let features = response.features;
+
+  // Initialize an array to hold earthquake markers.
+  let earthquakeMarkers = [];
+
+  // Loop through the features array.
+  for (let i = 0; i < features.length; i++) {
+   
+    let feature = features[i];
+    let lon = feature.geometry.coordinates[1];
+    let lat = feature.geometry.coordinates[0];
+    let depth = feature.geometry.coordinates[2];
+    let mag = feature.properties.mag * 40000;
+    
+    let earthquakeMarker = L.circle([lon,lat], {
+      fillOpacity: 0.75,
+      color: "grey",
+      fillColor: chooseColor(depth),
+      radius: mag,
+      weight: 1
+    }).bindPopup("<h3>" + feature.properties.place + "<h3><h3>Magnitude: " + feature.properties.mag +"<h3><h3>Depth: " + depth + "</h3>");
+  
+    // Add the marker to the eartquakeMarkers array.
+    earthquakeMarkers.push(earthquakeMarker);
+  }
+  // Create a layer group that's made from the earthquake markers array, and pass it to the createMap function.
+  createMap(L.layerGroup(earthquakeMarkers));
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# leaflet-challenge
+# Module 15 Challenge
+
+The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
+
+The USGS is interested in building a new set of tools that will allow them to visualise their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. In this challenge, you have been tasked with developing a way to visualise USGS data that will allow them to better educate the public and other government organisations (and hopefully secure more funding) on issues facing our planet.
+
+ ## Leaflet-Part-1 - Create Earthquake Visualisation 
+ 1. Choose a dataset to visualise - I chose  Past 7 days, all earthquakes data
+ 2. Use the URL of this JSON to pull in the data for the visualisation
+ 3. Import and visualise the data by doing the following:
+    * Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+    * Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by colour. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in colour.
+    * Include popups that provide additional information about the earthquake when its associated marker is clicked.
+    * Create a legend that will provide context for your map data.
+ 
+ 
+  ## Leaflet-Part-2 - Gather and Plot more data
+Plot a second dataset on your map to illustrate the relationship between tectonic plates and seismic activity. You will need to pull in this dataset and visualise it alongside your original data. Data on tectonic plates can be found at https://github.com/fraxen/tectonicplatesLinks to an external site..
+
+This part is completely optional.
+
+# References
+1. https://github.com/bahamas10/css-color-names/blob/master/css-color-names.json - I ended up using names rather than the reference codes 
+2. https://leafletjs.com/examples/quick-start/
+3. https://stackoverflow.com/questions/7878076/css-to-lay-out-keys-values-legend-horizontally -
