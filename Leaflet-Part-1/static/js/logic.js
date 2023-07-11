ecdd22b08d7be49f664c1a135e8eea044f908869
kirsty185 <126469546+kirsty185@users.noreply.github.com>
2023-07-11 19:42:10 +0800

    Add files via upload
    update to include topographic map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -9,9 +9,15 @@ let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+  
 // Create a baseMaps object.
 let baseMaps = {
   "Street Map": streetmap,
+  // Leaflet - Part 2
+  "Topographic Map": topo
  
 };
 
